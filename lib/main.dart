25d1986f608afe82d503ef819435a7afa8647e1b
Shahin Alom <shahinalomraj93@gmail.com>
2022-04-09 12:19:33 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,222 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: ListView(
+            children: [
+              ListTile(
+                title: Text('Easy Explanation'),
+                subtitle: Text('Learn everything with easy explanation'),
+                leading: CircleAvatar(
+                  child: Icon(Icons.message),
+                ),
+                trailing: CircleAvatar(
+                  child: Icon(Icons.add_a_photo),
+                ),
+              ),
+              ListTile(
+                title: Text('Easy Explanation'),
+                subtitle: Text('Learn everything with easy explanation'),
+                leading: CircleAvatar(
+                  child: Icon(Icons.message),
+                ),
+                trailing: CircleAvatar(
+                  child: Icon(Icons.add_a_photo),
+                ),
+              ),
+              ListTile(
+                title: Text('Easy Explanation'),
+                subtitle: Text('Learn everything with easy explanation'),
+                leading: CircleAvatar(
+                  child: Icon(Icons.message),
+                ),
+                trailing: CircleAvatar(
+                  child: Icon(Icons.add_a_photo),
+                ),
+              ),
+              ListTile(
+                title: Text('Easy Explanation'),
+                subtitle: Text('Learn everything with easy explanation'),
+                leading: CircleAvatar(
+                  child: Icon(Icons.message),
+                ),
+                trailing: CircleAvatar(
+                  child: Icon(Icons.add_a_photo),
+                ),
+              ),
+              ListTile(
+                title: Text('Easy Explanation'),
+                subtitle: Text('Learn everything with easy explanation'),
+                leading: CircleAvatar(
+                  child: Icon(Icons.message),
+                ),
+                trailing: CircleAvatar(
+                  child: Icon(Icons.add_a_photo),
+                ),
+              ),
+              ListTile(
+                title: Text('Easy Explanation'),
+                subtitle: Text('Learn everything with easy explanation'),
+                leading: CircleAvatar(
+                  child: Icon(Icons.message),
+                ),
+                trailing: CircleAvatar(
+                  child: Icon(Icons.add_a_photo),
+                ),
+              ),
+              ListTile(
+                title: Text('Easy Explanation'),
+                subtitle: Text('Learn everything with easy explanation'),
+                leading: CircleAvatar(
+                  child: Icon(Icons.message),
+                ),
+                trailing: CircleAvatar(
+                  child: Icon(Icons.add_a_photo),
+                ),
+              ),
+              ListTile(
+                title: Text('Easy Explanation'),
+                subtitle: Text('Learn everything with easy explanation'),
+                leading: CircleAvatar(
+                  child: Icon(Icons.message),
+                ),
+                trailing: CircleAvatar(
+                  child: Icon(Icons.add_a_photo),
+                ),
+              ),
+              ListTile(
+                title: Text('Easy Explanation'),
+                subtitle: Text('Learn everything with easy explanation'),
+                leading: CircleAvatar(
+                  child: Icon(Icons.message),
+                ),
+                trailing: CircleAvatar(
+                  child: Icon(Icons.add_a_photo),
+                ),
+              ),
+              ListTile(
+                title: Text('Easy Explanation'),
+                subtitle: Text('Learn everything with easy explanation'),
+                leading: CircleAvatar(
+                  child: Icon(Icons.message),
+                ),
+                trailing: CircleAvatar(
+                  child: Icon(Icons.add_a_photo),
+                ),
+              ),
+              ListTile(
+                title: Text('Easy Explanation'),
+                subtitle: Text('Learn everything with easy explanation'),
+                leading: CircleAvatar(
+                  child: Icon(Icons.message),
+                ),
+                trailing: CircleAvatar(
+                  child: Icon(Icons.add_a_photo),
+                ),
+              ),
+              ListTile(
+                title: Text('Easy Explanation'),
+                subtitle: Text('Learn everything with easy explanation'),
+                leading: CircleAvatar(
+                  child: Icon(Icons.message),
+                ),
+                trailing: CircleAvatar(
+                  child: Icon(Icons.add_a_photo),
+                ),
+              ),
+              ListTile(
+                title: Text('Easy Explanation'),
+                subtitle: Text('Learn everything with easy explanation'),
+                leading: CircleAvatar(
+                  child: Icon(Icons.message),
+                ),
+                trailing: CircleAvatar(
+                  child: Icon(Icons.add_a_photo),
+                ),
+              ),
+              ListTile(
+                title: Text('Easy Explanation'),
+                subtitle: Text('Learn everything with easy explanation'),
+                leading: CircleAvatar(
+                  child: Icon(Icons.message),
+                ),
+                trailing: CircleAvatar(
+                  child: Icon(Icons.add_a_photo),
+                ),
+              ),
+              ListTile(
+                title: Text('Easy Explanation'),
+                subtitle: Text('Learn everything with easy explanation'),
+                leading: CircleAvatar(
+                  child: Icon(Icons.message),
+                ),
+                trailing: CircleAvatar(
+                  child: Icon(Icons.add_a_photo),
+                ),
+              ),
+              ListTile(
+                title: Text('Easy Explanation'),
+                subtitle: Text('Learn everything with easy explanation'),
+                leading: CircleAvatar(
+                  child: Icon(Icons.message),
+                ),
+                trailing: CircleAvatar(
+                  child: Icon(Icons.add_a_photo),
+                ),
+              ),
+              ListTile(
+                title: Text('Easy Explanation'),
+                subtitle: Text('Learn everything with easy explanation'),
+                leading: CircleAvatar(
+                  child: Icon(Icons.message),
+                ),
+                trailing: CircleAvatar(
+                  child: Icon(Icons.add_a_photo),
+                ),
+              ),
+              ListTile(
+                title: Text('Easy Explanation'),
+                subtitle: Text('Learn everything with easy explanation'),
+                leading: CircleAvatar(
+                  child: Icon(Icons.message),
+                ),
+                trailing: CircleAvatar(
+                  child: Icon(Icons.add_a_photo),
+                ),
+              ),
+              ListTile(
+                title: Text('Easy Explanation'),
+                subtitle: Text('Learn everything with easy explanation'),
+                leading: CircleAvatar(
+                  child: Icon(Icons.message),
+                ),
+                trailing: CircleAvatar(
+                  child: Icon(Icons.add_a_photo),
+                ),
+              ),
+              ListTile(
+                title: Text('Easy Explanation'),
+                subtitle: Text('Learn everything with easy explanation'),
+                leading: CircleAvatar(
+                  child: Icon(Icons.message),
+                ),
+                trailing: CircleAvatar(
+                  child: Icon(Icons.add_a_photo),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
